db621b8cd3240669ef8417857b25bee13e381161
Jón Rúnar Helgason <dev@jonrh.is>
2022-03-24 15:46:35 -0700

    Preliminary logic for registering name and guessing in UI is in place

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,45 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 
-const inputStyle =
-  "my-3 py-2 px-4 " +
-  "border-2 border-gray-300 " +
-  "text-2xl text-center rounded " +
-  "form-control transition ease-in-out w-full";
+const labelStyle = `text-3xl font-bold`;
 
-const buttonStyle =
-  "py-4 px-4 " +
-  "text-2xl " +
-  "bg-gray-300 " +
-  "rounded w-full";
+const inputStyle = `
+  my-3 py-2 px-4
+  border-2 border-gray-300
+  text-2xl text-center rounded
+  form-control transition ease-in-out w-full
+`;
+
+const buttonStyle = `
+  py-4 px-4
+  text-2xl
+  bg-gray-300
+  rounded w-full
+`;
 
 export default function Index() {
   const [ready, setReady] = useState(false);
-  const [name, setName] = useState("");
+  const [playerName, setPlayerName] = useState("");
+  const [guess, setGuess] = useState("");
+  const [guesses, setGuesses] = useState([]);
+
+  const label = ready ? "Systir Nóa heitir:" : "Hvað heitir þú?";
+  const placeholder = ready ? "nafn" : "Dæmi: Jón Jónsson";
+  const buttonLabel = ready ? "Giska️" : "Áfram!";
+  const inputValue = ready ? guess : playerName;
+
+  const onInput = (event) => {
+    ready ? setGuess(event.target.value) : setPlayerName(event.target.value);
+  };
+
+  const onSubmit = () => {
+    if (ready) {
+      setGuesses(prevState => [...prevState, guess]);
+      setGuess("");
+    } else {
+      setReady(true);
+    }
+  };
 
   return (
     <>
@@ -23,31 +47,32 @@ export default function Index() {
         <title>Nafnaveisla 🎉</title>
       </Head>
 
-      <main className="text-center mx-5 my-16">
-        <p className="text-3xl font-bold">Hvað heitir þú?</p>
+      <main className="text-center mx-5 my-10">
+        <p className={labelStyle}>{label}</p>
 
         <input
           type="text"
-          value={name}
+          value={inputValue}
           autoFocus={true}
-          placeholder="Dæmi: Jón Jónsson"
+          placeholder={placeholder}
           className={inputStyle}
-          onChange={event => setName(event.target.value)}
+          onChange={onInput}
         />
 
         <div>
           <button
             type="button"
             className={buttonStyle}
-            onClick={() => setReady(true)}
+            onClick={onSubmit}
           >
-            Byrja!
+            {buttonLabel}
           </button>
         </div>
 
         {/* Debug: */}
-        {/*<p className="mt-5">Ready: {ready ? "true" : "false"}</p>*/}
-        {/*<p>Nafn: {name}</p>*/}
+        <p className="mt-5">Ready: {ready.toString()}</p>
+        <p>Player name: {playerName}</p>
+        <p>Guesses: {guesses.join(", ")}</p>
       </main>
     </>
   )
