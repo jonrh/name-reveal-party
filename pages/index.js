f5a0c71472066ccda14061dc433528ddb1610980
Jón Rúnar Helgason <dev@jonrh.is>
2022-03-24 13:18:24 -0700

    Minor font size change

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import Head from "next/head";
 const inputStyle =
   "m-3 px-4 py-2 " +
   "border border-gray-300 " +
-  "text-xl text-center rounded " +
-  "rounded transition ease-in-out";
+  "text-3xl text-center rounded " +
+  "form-control rounded transition ease-in-out";
 
-const buttonStyle = "bg-gray-300 py-2 px-4 rounded text-xl";
+const buttonStyle = "bg-gray-300 mx-5 py-2 px-4 rounded text-3xl w-full";
 
 export default function Index() {
   const [ready, setReady] = useState(false);
@@ -20,7 +20,7 @@ export default function Index() {
       </Head>
 
       <main className="text-center my-16">
-        <p className="text-3xl font-bold">Hvað heitir þú?</p>
+        <p className="text-6xl font-bold">Hvað heitir þú?</p>
 
         <input
           type="text"
