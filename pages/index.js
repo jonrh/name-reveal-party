f169587c7fc5df0607f293c302e56d341428223c
Jón Rúnar Helgason <dev@jonrh.is>
2022-03-24 13:11:32 -0700

    Added Tailwind CSS v3 with preliminary styling

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
 import React, {useState} from "react";
+import Head from "next/head";
+
+const inputStyle =
+  "m-3 px-4 py-2 " +
+  "border border-gray-300 " +
+  "text-xl text-center rounded " +
+  "rounded transition ease-in-out";
+
+const buttonStyle = "bg-gray-300 py-2 px-4 rounded text-xl";
 
 export default function Index() {
   const [ready, setReady] = useState(false);
@@ -6,20 +15,36 @@ export default function Index() {
 
   return (
     <>
-      <p>Hvað heitir þú?</p>
-      <input
-        type="text"
-        value={name}
-        autoFocus={true}
-        placeholder="Jón Jónsson"
-        onChange={event => setName(event.target.value)}
-      />
-      <br />
-      <button onClick={() => setReady(true)}>Byrja!</button>
-      <br />
-      Ready: {ready ? "true" : "false"}
-      <br />
-      Nafn: {name}
+      <Head>
+        <title>Nafnaveisla 🎉</title>
+      </Head>
+
+      <main className="text-center my-16">
+        <p className="text-3xl font-bold">Hvað heitir þú?</p>
+
+        <input
+          type="text"
+          value={name}
+          autoFocus={true}
+          placeholder="Jón Jónsson"
+          className={inputStyle}
+          onChange={event => setName(event.target.value)}
+        />
+
+        <div>
+          <button
+            type="button"
+            className={buttonStyle}
+            onClick={() => setReady(true)}
+          >
+            Byrja!
+          </button>
+        </div>
+
+        {/* Debug: */}
+        {/*<p className="mt-5">Ready: {ready ? "true" : "false"}</p>*/}
+        {/*<p>Nafn: {name}</p>*/}
+      </main>
     </>
   )
 }
