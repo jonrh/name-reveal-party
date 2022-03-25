f106c40a01edab26894f73b84d7c4b10bd51e7be
Jón Rúnar Helgason <dev@jonrh.is>
2022-03-25 13:13:59 -0700

    Fixed a Next.js warning about improper font style inclusion in header

--- pages/index.js
+++ pages/index.js
@@ -73,9 +73,6 @@ export default function Index() {
     <>
       <Head>
         <title>Nafnaveisla 🎉</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet" />
       </Head>
 
       {/* Background image */}
